0f1335434198b5bfc03c484893dd57f660a83302
yongkim93 <yong4@hawaii.edu>
2021-06-02 14:11:44 -1000

    complete modal

--- src/containers/calendar/Calendar.tsx
+++ src/containers/calendar/Calendar.tsx
@@ -1,12 +1,14 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./Calendar.scss";
 import { useWindowSizeManager } from "~/utility/WindowSizeManager";
 import { RowHeader } from "~/components/row_header/RowHeader";
 import { ColHeader } from "~/components/col_header/ColHeader";
 import { Grid } from "~/components/grid/Grid";
 import DragOnGrid from "~/components/drag_on_grid/DragOnGrid";
+import Modal from "~/components/modal/Modal";
 
 export function Calendar() {
+  const [isActive, setIsActive] = useState<boolean>(true);
   return (
     <Fragment>
       <div className="column-align">
@@ -18,6 +20,13 @@ export function Calendar() {
           </div>
         </div>
       </div>
+      <Modal
+        isActive={isActive}
+        onClose={() => setIsActive(false)}
+        onSubmit={() => setIsActive(false)}
+      >
+        <div></div>
+      </Modal>
       <DragOnGrid />
     </Fragment>
   );
